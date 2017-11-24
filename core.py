d8c48fc5d51132b4ab59d809a174ca5026ffadba
Sam Drew <sam-drew@outlook.com>
2017-11-24 13:49:18 +0000

    Now checks if JSON and URL's are formatted correctly. And works for POST

--- core.py
+++ core.py
@@ -7,7 +7,6 @@ import logging
 import os.path
 import pycurl
 from io import BytesIO
-import bcrypt
 import requests
 import json
 
@@ -16,20 +15,34 @@ class RootHandler(tornado.web.RequestHandler):
         self.render("index.html")
     def post(self):
         request_url = self.get_argument("url")
-        request_json = self.get_argument("json")
+        request_json_string = self.get_argument("json")
         request_method = self.get_argument("method-select")
         if request_method == "GET":
             # Do get request to url and print response
-            r = requests.get(request_url)
             try:
-                response_body = r.json()
-                self.render("response.html", response = response_body, original_url = request_url, original_json = request_json, errors = None)
-            except ValueError as e:
-                self.render("response.html", response = r, original_url = request_url, original_json = request_json, errors = "Response body was not in JSON")
+                r = requests.get(request_url)
+                try:
+                    response_body = r.json()
+                    self.render("response.html", response = response_body, original_url = request_url, original_json = request_json_string, errors = None)
+                except ValueError as e:
+                    self.render("response.html", response = r, original_url = request_url, original_json = request_json_string, errors = "Response body was not in JSON")
+            except Exception as e:
+                self.render("response.html", response = "", original_url = request_url, original_json = request_json_string, errors = "URL not formatted properly")
         elif request_method == "POST":
             # Do post request and print response
-            pass
-        self.render("response.html", response = "The internet am I right", original_url = request_url, original_json = request_json)
+            try:
+                request_json = json.loads(request_json_string)
+                try:
+                    r = requests.post(request_url, json = request_json)
+                    try:
+                        response_body = r.json()
+                        self.render("response.html", response = response_body, original_url = request_url, original_json = request_json_string, errors = None)
+                    except ValueError as e:
+                        self.render("response.html", response = r, original_url = request_url, original_json = request_json_string, errors = "Response body was not in JSON")
+                except Exception as e:
+                    self.render("response.html", response = "", original_url = request_url, original_json = request_json_string, errors = "URL not formatted properly")
+            except Exception as e:
+                self.render("response.html", response = r, original_url = request_url, original_json = request_json_string, errors = "JSON was not correctly fomatted")
 
 app = tornado.web.Application(
     [(r"/", RootHandler),],
